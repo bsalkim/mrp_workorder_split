7c583869a37475ace56ce53216311636e908b002
bsalkim <bora@globescout.com.tr>
2025-06-27 21:23:18 +0300

    Update mrp_workorder.py

--- mrp_workorder_split/models/mrp_workorder.py
+++ mrp_workorder_split/models/mrp_workorder.py
@@ -1,10 +1,13 @@
-
 from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
     def record_production(self):
+        _logger.info(f"💡 [MODÜL ÇALIŞTI] Workorder: {self.name} - Produced: {self.qty_produced} / Planned: {self.qty_to_produce}")
         res = super().record_production()
         for workorder in self:
             produced_qty = workorder.qty_produced
@@ -12,6 +15,7 @@ class MrpWorkorder(models.Model):
 
             if produced_qty < planned_qty:
                 remaining_qty = planned_qty - produced_qty
+                _logger.info(f"⏳ Yeni iş emri oluşturuluyor: Kalan {remaining_qty} adet")
                 self.env['mrp.workorder'].create({
                     'production_id': workorder.production_id.id,
                     'operation_id': workorder.operation_id.id,
