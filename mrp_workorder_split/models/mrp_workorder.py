5b1623ab0d26bdc000a2403f287fd1c0f8ab21a1
bsalkim <bora@globescout.com.tr>
2025-06-27 22:42:59 +0300

    Update mrp_workorder.py

--- mrp_workorder_split/models/mrp_workorder.py
+++ mrp_workorder_split/models/mrp_workorder.py
@@ -13,7 +13,7 @@ class MrpWorkorder(models.Model):
 
         for workorder in self:
             produced_qty = workorder.qty_produced
-            planned_qty = workorder.qty_to_produce
+            planned_qty = workorder.production_id.product_qty
 
             if produced_qty < planned_qty:
                 remaining_qty = planned_qty - produced_qty
@@ -24,8 +24,8 @@ class MrpWorkorder(models.Model):
                     'production_id': workorder.production_id.id,
                     'operation_id': workorder.operation_id.id,
                     'workcenter_id': workorder.workcenter_id.id,
-                    'qty_to_produce': remaining_qty,
                     'qty_produced': 0,
+                    'state': 'ready',  # işleme hazır hale getirebilirsin
                 })
 
         return res
